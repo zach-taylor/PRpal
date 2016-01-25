bea533a6ada1c3f474ce9b6c15bb590000916e9a
Zach Taylor <ztaylor234@gmail.com>
2016-01-24 21:32:41 -0600

    Revert routes change

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  #post 'payloads/:service', to: 'payloads#create'
-
-  resources :payloads, only: :create, defaults: { formats: :json }
+  post 'payloads/:service', to: 'payloads#create'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
