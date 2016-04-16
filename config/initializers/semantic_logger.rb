a231953e1c3180cc60a91966e86201fd20cf998f
Zach Taylor <ztaylor234@gmail.com>
2016-04-16 14:22:10 -0500

    Add logentries forwarding

---
+++ config/initializers/semantic_logger.rb
@@ -0,0 +1,24 @@
+SemanticLogger.application = 'PRpal'
+
+if Rails.env.production?
+  module PRpal
+    module Formatters
+      class Logentries < SemanticLogger::Formatters::Json
+        def call(log, logger)
+          "#{Rails.application.secrets.logentries_token} #{super(log, logger)}"
+        end
+      end
+    end
+  end
+
+  Rails.application.config.after_initialize do
+    formatter = PRpal::Formatters::Logentries.new
+    Rails.application
+      .config
+      .semantic_logger
+      .add_appender(appender: :tcp,
+                    server: 'api.logentries.com:20000',
+                    ssl: true,
+                    formatter: formatter)
+  end
+end
