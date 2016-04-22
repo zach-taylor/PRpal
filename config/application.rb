865b1f81abf680a0d9786c8dbbb19ff168d3e328
Zach Taylor <ztaylor234@gmail.com>
2016-04-21 20:47:29 -0500

    Attempt temporary workaround for semantic logger

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'action_controller/log_subscriber'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
